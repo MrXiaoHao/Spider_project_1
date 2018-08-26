00020be642f95db42af11c36eb28657ef9a1d2c4
MrXiaoHao <735072257@qq.com>
2018-08-26 10:20:54 +0800

    修改标题

---
+++ dt_spider.py
@@ -0,0 +1,14 @@
+import requests
+import re
+from requests.exceptions import RequestException
+
+url =""
+def get_index():
+    pass
+def parse_html():
+    pass
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
